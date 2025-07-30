f659d07e2a3ac907cc4be091211dec1cca2278e8
Bogdan5ik <baggi.juk@mail.ru>
2025-07-30 15:01:38 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Главная</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta charset="utf-8">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+<?php $tweet1 = [
+	"name"=>"Меланхоличка",
+	"text"=>"Самая страшная и мистическая история произошла со мной в квартире когда я спала и сама по себе косметичка упала на пол. Хотя она стояла на поддоконике далеко от края. Форточка была закрыта.",
+	"avatar"=>"img/2.png",
+	"image"=>"img/image.jpg",
+]     
+?>
+<body class="">												
+	<div class="main mt-3">
+		<div class="container">
+			<div class="row">
+				<!-- левая колонка --> 
+				<div class="col-3">
+					<div class="mt-3">
+						<h4 class="fw-normal">Главная</h4>
+					</div>
+					<div class="mt-4">
+						<h4 class="fw-normal">Обзор</h4>
+					</div>
+					<div class="mt-4">
+						<h4 class="fw-normal">Уведомления</h4>
+					</div>
+					<div class="mt-4">
+						<h4 class="fw-normal">Сообщения</h4>
+					</div>
+					<div class="mt-4">
+						<h4 class="fw-normal">Закладки</h4>
+					</div>
+					<div class="mt-4">
+						<h4 class="fw-normal">Списки</h4>
+					</div>
+					<div class="mt-4">
+						<h4 class="fw-normal">Профиль</h4>
+					</div>
+					<div class="mt-4">
+						<button class="rounded-pill btn btn-primary w-75 py-2">Твитнуть</button>
+					</div>
+					
+				</div>
+
+				<!-- Средняя колонка -->
+				<div class="col-6 border-end border-start">
+					<!--Добавить твит форма-->
+					<div class="pt-2 bg-white">						
+						<div class="row">							
+							<div class="col-1">
+								
+								<img name="img" src="img/1.jpg" class="rounded-circle">
+							</div>							
+							<div class="col-10">
+								<div class="col-12">
+									<form action="index.php" method="GET">
+
+									<input name="name" type="text" class="form-control" placeholder="Автор">
+									<textarea name="text" class="form-control mt-2" placeholder="Что нового?"></textarea>
+									<button type="submit" class="btn btn-primary mt-2">Твитнуть</button>
+
+									</form>
+								</div>								
+							</div>
+						</div>				
+							
+					</div>
+
+					<!--Вывод постов тут-->
+					<div class="pt-2 bg-while">
+
+					   <div class="row mt-2 border-top border-bottom py-3">
+							<div class="col-2">
+								<img src="img/1.jpg" class="rounded-circle w-75" alt="">
+						</div>
+							<div class="col-10">
+								<h6><?php echo $_GET['name']?></h6>
+								<p><?php echo $_GET['text']?></p>
+							</div>
+						</div>
+
+						<div class="row mt-2 border-top border-bottom py-3">
+							<div class="col-2">
+								<img src="<?php echo $tweet1['avatar']?>" class="rounded-circle w-75" alt="">
+						</div>
+							<div class="col-10">
+								<h6><?php echo $tweet1['name']?></h6>
+
+								<p><?php echo $tweet1['text']?></p>
+								<img src="<?php echo $tweet1['image']?>" class="rounded-circle w-75" alt="">
+							</div>
+						</div>
+					</div>
+					
+				</div>
+
+				<!--Правая колонка-->
+				<div class="col-3">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda magnam ut quos veritatis laborum beatae earum. Facere atque voluptatum ad repudiandae nesciunt! Temporibus inventore repudiandae facilis nulla, cum omnis voluptate.
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
